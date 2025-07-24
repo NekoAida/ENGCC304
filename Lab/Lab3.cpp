51cdece553265f4d72b8328e94805ec615b2f8ef
NekoAida <toffyrock10@gmail.com>
2025-07-24 23:21:42 +0700

    Lab3

--- Lab/Lab3.cpp
+++ Lab/Lab3.cpp
@@ -5,34 +5,35 @@ int isPrime(int num) {
   if(num<2)return 0;
   for (int i = 2; i <= sqrt(num); i++) {
       if (num % i == 0)return 0;
-  }
+  }//End if
   return 1;}
-int main() {
-    int n;
+    int main() {
+        int n;
 
-    printf("Enter N : ");
-    scanf("%d",&n);
+        printf("Enter N : ");
+        scanf("%d",&n);
 
-    int arr[n]; 
+        int arr[n]; 
     
-    for(int i=0;i<n;i++) {
-        printf("Enter value[%d] : ",i);
-        scanf("%d",&arr[i]);
-      }
-    printf("Index:  ");
-    for (int i = 0;i < n;i++) {
-        printf("%2d ", i);
-    }
-    printf("\n");
+        for(int i=0;i<n;i++) {
+            printf("Enter value[%d] : ",i);
+            scanf("%d",&arr[i]);
+        }//End for
 
-    printf("Array:  ");
-    for(int i=0;i<n;i++) {
-        if (isPrime(arr[i]))
+        printf("Index:  ");
+        for (int i = 0;i < n;i++) {
+            printf("%2d ", i);
+        }//End for
+        printf("\n");
+
+        printf("Array:  ");
+        for(int i=0;i<n;i++) {
+            if (isPrime(arr[i]))
             printf("%2d ", arr[i]); 
-        else
+            else
             printf("%2s ", "#"); 
-    }
-    printf("\n");
+        }//End for
+        printf("\n");
     
-    return 0;
-}
+        return 0;
+}//End main
