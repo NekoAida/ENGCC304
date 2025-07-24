6137ed98fff953f9ac13c468d7d3667c93aa12fe
NekoAida <toffyrock10@gmail.com>
2025-07-24 23:42:23 +0700

    Lab4

---
+++ Lab/Lab4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char ID[11];
+    int hrs;
+    float salary;
+    printf("Input the Employees ID(Max. 10 chars):");
+    scanf("%10s", ID);
+    printf("Input the working hrs: ");
+    scanf("%d", &hrs);
+    printf("Salary amount/hr: ");
+    scanf("%f", &salary);
+
+    printf("Expected Output:\n");
+    printf("Employees ID: %s\n", ID);
+    printf("Salary = U$ %.2f\n", (float)hrs * salary);
+}
